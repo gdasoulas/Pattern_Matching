a9d9a10fa9246046ad20055e0d8e8df7f1eeed06
George Dasoulas <geodas94@gmail.com>
2016-10-26 20:55:28 +0300

    Commit message

--- Code.m
+++ Code.m
@@ -1,3 +1,5 @@
+%% First Lab - After first 9 questions ...
+
 fprintf('First Questions: Loading train.txt ,test.txt and computing m_all,s_all \n');
 
 A = load('train.txt');			% opening training file
@@ -25,7 +27,7 @@ end
 
 fprintf('--------------------------------\n 11th question ...\n Bayesian Classifier\n');
 
-%s_all_biased = s_all + (1/(4*pi+3)); % biased with 1/2π
+%s_all_biased = s_all + (1/2*pi); % biased with 1/2π
 s_all_biased = s_all + (1/(4*pi+3)); % biased with 1/(4π+3) for better estimation 
 
 Px_depC = dependent_prob(TestData,m_all,s_all_biased);
@@ -57,6 +59,7 @@ for j=1:size(TestData,1)
     end
 
 p_bayes = find(TestData(:,1) == (idx(:)-1) );		% finding correct matches
+p_fail = find(TestData(:,1) ~= (idx(:)-1) )
 fprintf('Success rate for Bayes : %f%%\n',size(p_bayes,1)/size(TestData,1)*100 );
 
 %% Bhma 12
@@ -95,4 +98,24 @@ for j=1:size(TestData,1)
 
 p_bayes = find(TestData(:,1) == (idx(:)-1) );		% finding correct matches
 fprintf('Success rate for Bayes with Var=one : %f%%\n',size(p_bayes,1)/size(TestData,1)*100 );
+
+%% Bhma 13
+
+%Nearest neighbor 
+for i=1:size(TestData)
+    for j=1:size(A)
+        Eu_dist(i,j) = (sum((TestData(i,2:end)-A(j,2:end)).^2));
+    end
+end
+
+%%
+counter = 0
+for i=1:size(TestData)
+    [~,idx] = min(Eu_dist(i,:));
+    if A(idx,1) == TestData(i,1)
+        counter =counter+1
+    end
+end
+
+fprintf('Success rate for 1nn : %f%%\n',counter/size(TestData,1)*100 );
 
