ecc987b738b1a8f47fa48d76cee0fbbc09c1d20d
George Dasoulas <geodas94@gmail.com>
2016-11-06 23:41:18 +0200

    Added neural network classifier

---
+++ nn_classifier.py
@@ -0,0 +1,54 @@
+from sknn.mlp import Classifier, Layer
+import numpy as np
+import logging
+
+logging.basicConfig()
+
+
+TrainData=[]
+TestData=[]
+Classes = range(0,10)
+
+with open("train.txt") as textFile:
+    lines = [line.split() for line in textFile]
+    for line in lines:
+    	int_line=[float(x) for x in line]
+	TrainData.append(int_line)
+
+TrainData = np.array(TrainData)
+
+with open("test.txt") as textFile:
+    lines = [line.split() for line in textFile]
+    for line in lines:
+    	int_line=[float(x) for x in line]
+	TestData.append(int_line)
+
+TestData = np.array(TestData)
+
+nn = Classifier(
+    layers=[
+        Layer("Sigmoid", units=100),
+        Layer("Softmax")],
+    learning_rate=0.02,
+    n_iter=10)
+
+
+X_train = TrainData[:,1:]
+y_train = TrainData[:,0]
+
+new_y_train = np.zeros((len(X_train),len(Classes)))
+
+for i in range(0,len(TrainData)):
+	new_y_train[i,int(y_train[i])]=1
+
+nn.fit(X_train,y_train)
+
+
+#y_valid = nn.predict(TestData[:,1:])
+
+X_test = TestData[:,1:] 
+y_test = TestData[:,0]
+score = nn.score(X_test, y_test)
+
+print score
+
