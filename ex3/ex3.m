77f04d221fe07c94355d48aa753d5ca295b9e389
George Dasoulas <geodas94@gmail.com>
2016-12-17 21:07:56 +0200

    Cooccurence

--- ex3/ex3.m
+++ ex3/ex3.m
@@ -1,11 +1,29 @@
 clear all
 clc
 
-%%
+%% bhma 1 
 
 names = dir('./PRcourse_Lab3_data/MusicFileSamples/*.wav');
 for i=1:10
     music_file(i)=Sound(names(i).name);
 end
 
+clear i names
+
+%% bhma 2 
+
+
+[val,act,cooccurence,~]=stats();
+
+for i=1:3
+    mean_Valence(i) = mean(val(i,:));
+    std_Valence(i) = std(val(i,:));
+    mean_Activation(i) = mean(act(i,:));
+    std_Activation(i) = std(act(i,:));
+end
+
 clear i 
+
+
+
+
---
+++ ex3/stats.m
@@ -0,0 +1,30 @@
+function [val,act,cooccurence,labels]=stats()
+    dir_emotion = './PRcourse_Lab3_data/EmotionLabellingData/';
+    labels(1) = load(strcat(dir_emotion,'Labeler1.mat'));
+    labels(2) = load(strcat(dir_emotion,'Labeler2.mat'));
+    labels(3) = load(strcat(dir_emotion,'Labeler3.mat'));
+
+    for i=1:3
+        for j=1:size(labels(i).labelList,1)
+            val(i,j)=labels(i).labelList(j).valence;
+            act(i,j)=labels(i).labelList(j).activation;
+        end
+    end
+
+    for i=1:3 
+        X=[val(i,:)' act(i,:)']; 
+        n = zeros(5,5);
+        for j=1:size(X,1)
+            n(X(j,1),X(j,2))=n(X(j,1),X(j,2))+1;
+        end
+        cooccurence{i}=n;
+        xb = linspace(min(X(:,1)),max(X(:,1)),size(n,1));
+        yb = linspace(min(X(:,2)),max(X(:,2)),size(n,1));
+        figure;
+        h = pcolor(xb,yb,n);
+        xlabel('Valence');
+        ylabel('Activation');
+        title(strcat('2D Histogram of Labels for Annotator No ',num2str(i)));
+
+    end
+end
