7bd9440c0da78c4ae1afc83e5e37f9795fc44c33
George Dasoulas <geodas94@gmail.com>
2016-12-17 21:42:23 +0200

    Plots - histograms for differences

--- ex3/ex3.m
+++ ex3/ex3.m
@@ -13,7 +13,7 @@ clear i names
 %% bhma 2 
 
 
-[val,act,cooccurence,~]=stats();
+[val,act,cooccurence,labels]=stats();
 
 for i=1:3
     mean_Valence(i) = mean(val(i,:));
@@ -24,6 +24,29 @@ end
 
 clear i 
 
+
+%% bhma 3 
+
+[agree_val,agree_act,diff_val,diff_act]= agreement(val,act);
+
+for i=1:2
+    for j=i+1:3
+%         figure;
+        hist(squeeze(diff_val(i,j,:)));
+        xlabel('Difference I_{ij}-I{ik}');
+        ylabel('Occurences');
+        title(strcat('Histogram for Valence between No',num2str(i),' and No',num2str(j),' Annotators'));
+        saveas(gcf,strcat('diff_valence_',num2str(i),'_',num2str(j),'.png'));
+        
+%         figure;
+        hist(squeeze(diff_val(i,j,:)));
+        xlabel('Difference I_{ij}-I{ik}');
+        ylabel('Occurences');
+        title(strcat('Histogram for Activation between No',num2str(i),' and No',num2str(j),' Annotators'));
+        saveas(gcf,strcat('diff_activation_',num2str(i),'_',num2str(j),'.png'));
+
+    end
+end
 
 
 
